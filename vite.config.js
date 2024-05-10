6f6caa5cda835b316f09e9494a936447f72c55d8
rmmwalali <2003285@students.kcau.ac.ke>
2024-05-10 22:37:32 +0300

    lessbaseurl

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'http://patnaninvestment.co.ke/',
+  // base: 'http://patnaninvestment.co.ke/',
   plugins: [react()],
   server: {
     watch: {
