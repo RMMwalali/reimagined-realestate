c4d52ae4259767d0ecb9155e2aa4160dd93c679a
rmmwalali <2003285@students.kcau.ac.ke>
2024-05-17 17:31:08 +0300

    great

--- src/showcase2.jsx
+++ src/showcase2.jsx
@@ -5,7 +5,6 @@ import Showcase2Card from "../components/showcase2-components/showcase2Card";
 import { showCase } from "../constants/showcase";
 import ShowCase2HeroSection from "../components/showcase2-components/showcase2HeroSecion";
 
-
 const ShowCase2 = () => {
   const [tabIndex, setTabIndex] = useState(0);
 
@@ -17,17 +16,18 @@ const ShowCase2 = () => {
     <div className="overflow-hidden">
       <ShowCase2HeroSection />
       <Tabs selectedIndex={tabIndex} onSelect={index => setTabIndex(index)}>
-      <TabList className="flex justify-center p-2 bg-white rounded-lg">
-        <Tab className="mx-2 py-3 px-6 text-xl title-font font-extrabold text-white bg-red-500 border-red-500 border-2 rounded-t-lg hover:bg-white hover:border-red-500 hover:text-red-500">
-          Land
-        </Tab>
-        <Tab className="mx-2 py-3 px-6 text-xl title-font font-extrabold text-red-500 bg-white border-red-500 border-2 rounded-t-lg hover:text-white hover:bg-red-500 hover:border-red-500">
-          Site Developments
-        </Tab>
-      </TabList>
+        <TabList className="flex justify-center p-2 bg-white rounded-lg">
+          <Tab className="mx-2 py-3 px-6 text-xl title-font font-extrabold text-white bg-red-500 border-red-500 border-2 rounded-t-lg hover:bg-white hover:border-red-500 hover:text-red-500">
+            Land
+          </Tab>
+          <Tab className="mx-2 py-3 px-6 text-xl title-font font-extrabold text-red-500 bg-white border-red-500 border-2 rounded-t-lg hover:text-white hover:bg-red-500 hover:border-red-500">
+            Site Developments
+          </Tab>
+        </TabList>
+
         <TabPanel>
           {/* Land images */}
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {landImages.map((e, i) => (
               <Showcase2Card
                 href={`/projects/${e.id}`}
@@ -40,7 +40,7 @@ const ShowCase2 = () => {
 
         <TabPanel>
           {/* Site Development images */}
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {siteDevelopmentImages.map((e, i) => (
               <Showcase2Card
                 href={`/projects/${e.id}`}
